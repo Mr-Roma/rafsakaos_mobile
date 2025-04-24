c4f93e987c35e0d08896927cc71d89274ef8ec19
Muhammad Sirojul Fuad <94115640+Sirojulf@users.noreply.github.com>
2025-04-24 21:00:18 +0700

    add search bar to home

--- lib/presentation/pages/homepage.dart
+++ lib/presentation/pages/homepage.dart
@@ -6,10 +6,40 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          'This is Home Page',
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children:  const[
+                  Text(
+                    "Hi, Selamat Datang",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF4C4DDC)
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 16),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "Cari Bahan",
+                  prefixIcon: Icon(Icons.search),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: BorderSide(),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16),
+
+            ],
+          ),
         ),
       ),
     );
